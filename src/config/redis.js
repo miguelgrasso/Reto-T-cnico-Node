a43950a34ab2d1893455ee3af846dd6ec7172fae
miguelgrasso <44530038+miguelgrasso@users.noreply.github.com>
2023-11-24 13:00:24 -0500

    Delete src/config/redis.js

--- src/config/redis.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clientDb = void 0;
-const redis_1 = require("redis");
-const clientDb = (0, redis_1.createClient)();
-exports.clientDb = clientDb;
